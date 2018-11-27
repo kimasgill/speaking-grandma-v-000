855d1beb710bd8f9aad27dd43e0d2461ec2cafcf
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-27 06:26:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
-  puts "NO, NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
   elsif !(phrase.upcase)
-  puts "HUH?! SPEAK UP, SONNY!"
+   "HUH?! SPEAK UP, SONNY!"
 end
 end
 
